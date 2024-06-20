87e42b50e1cfb9968ee41146b10404ca934e0479
Alex Argueta <alexarguetanet@gmail.com>
2024-06-20 14:51:45 -0400

    Added connect method to app entry point

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
-app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
-});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+
+    app.listen(PORT, () => {
+        console.log(`Connected to Mongodb. Server running on PORT ${PORT}`);
+    });
+
+})
+.catch(err => console.log(err.message));
+
